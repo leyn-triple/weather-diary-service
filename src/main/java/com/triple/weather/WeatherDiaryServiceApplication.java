2409e8efaa265f4efcc41ad1036a10294a59a0de
leyn <leyn53.triple@gmail.com>
2023-10-18 18:21:10 +0900

    Initial commit

---
+++ src/main/java/com/triple/weather/WeatherDiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.triple.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDiaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDiaryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/triple/weather/WeatherDiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDiaryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

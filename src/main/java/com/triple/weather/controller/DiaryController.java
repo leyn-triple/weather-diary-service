01bb4c58e89206a29d01b86c3f64f16d867343be
LEYN <leyn53.triple@gmail.com>
2023-10-23 13:03:46 +0900

    날씨 데이터 CRUD 구현

--- src/main/java/com/triple/weather/controller/DiaryController.java
+++ src/main/java/com/triple/weather/controller/DiaryController.java
@@ -7,12 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,27 +15,45 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    /**
+     *날짜에 따른 일기 생성
+     */
     @PostMapping("/create/diary")
     void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
-    //일기 조회
+    /**
+     *특정 하루 일기 조회
+     */
     @GetMapping("/read/diary")
     List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
         return diaryService.readDiary(date);
     }
 
-    //기간의 모든 일기 조회
+    /**
+     *기간의 모든 일기 조회
+     */
     @GetMapping("/read/diaries")
     List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate startDate,
                             @RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate endDate){
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    /**
+     *이미 쓴 일기 수정
+     */
     @PutMapping("/update/diary")
     void updateDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date, @RequestBody String text) {
         diaryService.updateDiary(date, text);
+    }
+
+    /**
+     *이미 쓴 일기 삭제
+     */
+    @DeleteMapping("/delete/diary" )
+    void deleteDiary(@RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
+        diaryService.deleteDiary(date);
     }
 
 }
--- src/main/java/com/triple/weather/repository/DiaryRepository.java
+++ src/main/java/com/triple/weather/repository/DiaryRepository.java
@@ -6,12 +6,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer>{
-    List<Diary> findAllByDate(LocalDate date);
+    List<Diary> findAllByDate(LocalDate       date);
 
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
     Diary getFirstByDate(LocalDate date);
+
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
